a7051454e4d0f3aeb67407d80d3ec5f4a8b42b27
Zatsune No Mokou <ZatsuneNoMokou@users.noreply.github.com>
2023-09-02 21:58:52 +0200

    update

--- update.bat
+++ update.bat
@@ -1,5 +1,9 @@
 @echo off
 
+git stash
+git pull
+git stash pop
+
 git -C "%~dp0webui" stash
 git -C "%~dp0webui" pull
 git -C "%~dp0webui" stash pop
--- update.sh
+++ update.sh
@@ -2,6 +2,10 @@
 
 __dirname=$(dirname $(readlink -f $0))
 
+git stash
+git pull
+git pop
+
 git -C "${__dirname}/webui" stash
 git -C "${__dirname}/webui" pull
 git -C "${__dirname}/webui" pop
