db6b8f7500083099f32d1039d1c2ffd5f3c0483d
Zatsune No Mokou <ZatsuneNoMokou@users.noreply.github.com>
2023-09-03 21:08:58 +0200

    update dir

--- update.bat
+++ update.bat
@@ -1,8 +1,8 @@
 @echo off
 
-git stash
-git pull
-git stash pop
+git -C "%~dp0" stash
+git -C "%~dp0" pull
+git -C "%~dp0" stash pop
 
 git -C "%~dp0webui" stash
 git -C "%~dp0webui" pull
--- update.sh
+++ update.sh
@@ -2,9 +2,9 @@
 
 __dirname=$(dirname $(readlink -f $0))
 
-git stash
-git pull
-git pop
+git -C "${__dirname}" stash
+git -C "${__dirname}" pull
+git -C "${__dirname}" pop
 
 git -C "${__dirname}/webui" stash
 git -C "${__dirname}/webui" pull
